2150eb490e84e0af7a03a96c87fe166641c70c47
Davanum Srinivas <dims@apache.org>
2006-06-24 02:54:05 +0000

    Fix for WSCOMMONS-47 - additional schema tests
    git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/trunk/modules/XmlSchema@416855 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/tests/ChoiceTest.java
@@ -0,0 +1,115 @@
+package tests;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.HashSet;
+
+import javax.xml.namespace.QName;
+import javax.xml.transform.stream.StreamSource;
+
+import junit.framework.TestCase;
+
+import org.apache.ws.commons.schema.XmlSchema;
+import org.apache.ws.commons.schema.XmlSchemaType;
+import org.apache.ws.commons.schema.XmlSchemaCollection;
+import org.apache.ws.commons.schema.XmlSchemaChoice;
+import org.apache.ws.commons.schema.XmlSchemaComplexType;
+import org.apache.ws.commons.schema.XmlSchemaElement;
+import org.apache.ws.commons.schema.XmlSchemaObjectCollection;
+
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ * Copyright 2006 International Business Machines Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+public class ChoiceTest extends TestCase {
+
+    /**
+     * This method will test the choice.
+     *
+     * @throws Exception Any exception encountered
+     */
+    public void testChoice() throws Exception {
+
+        /*
+        <schema xmlns="http://www.w3.org/2001/XMLSchema"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                xmlns:tns="http://soapinterop.org/types"
+                targetNamespace="http://soapinterop.org/types">
+
+          <element name="computer">
+            <complexType>
+              <choice>
+                <element name="desktop" type="string"/>
+                <element name="laptop" type="string"/>
+              </choice>
+            </complexType>
+          </element>
+
+        </schema>
+        */
+
+        QName ELEMENT_QNAME = new QName("http://soapinterop.org/types",
+                                        "computer");
+        InputStream is = new FileInputStream("test-resources/choice.xsd");
+        XmlSchemaCollection schemaCol = new XmlSchemaCollection();
+        XmlSchema schema = schemaCol.read(new StreamSource(is), null);
+
+        QName WRONG_QNAME = new QName("http://soapinterop.org/types",
+                                      "machine");
+        XmlSchemaElement elem = schemaCol.getElementByQName(WRONG_QNAME);
+        assertNull(elem);
+        elem = schemaCol.getElementByQName(ELEMENT_QNAME);
+        assertEquals("computer", elem.getName());
+        assertEquals(new QName("http://soapinterop.org/types", "computer"),
+                     elem.getQName());
+
+        XmlSchemaComplexType cType = (XmlSchemaComplexType)elem.getSchemaType();
+        assertNotNull(cType);
+
+        XmlSchemaChoice choice = (XmlSchemaChoice)cType.getParticle();
+        assertNotNull(choice);
+
+        Set s = new HashSet();
+        s.add("desktop");
+        s.add("laptop");
+        XmlSchemaObjectCollection items = choice.getItems();
+        Iterator iterator = items.getIterator();
+        while (iterator.hasNext()) {
+            XmlSchemaElement e = (XmlSchemaElement)iterator.next();
+            String eName = e.getName();
+            if (eName.equals("desktop")) {
+                assertEquals(new QName("", "desktop"), e.getQName());
+                assertEquals(e.getName(), "desktop");
+            } else if (eName.equals("laptop")) {
+                assertEquals(new QName("", "laptop"), e.getQName());
+                assertEquals(e.getName(), "laptop");
+            } else {
+                fail("Should have had a name of desktop or laptop, but"
+                     + " instead had " + eName);
+            }
+            assertEquals(new QName("http://www.w3.org/2001/XMLSchema",
+                                   "string"), e.getSchemaTypeName());
+            assertTrue(s.remove(e.getName()));
+        }
+        assertTrue("The set should have been empty, but instead contained: "
+                   + s + ".",
+                   s.isEmpty());
+    }
+
+}
