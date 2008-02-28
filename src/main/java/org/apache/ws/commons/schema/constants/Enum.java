244500482a8f2d74052e6ea7f3ef483aca336227
Deepal Jayasinghe <deepal@apache.org>
2008-02-28 11:00:10 +0000

    fixing https://issues.apache.org/jira/browse/WSCOMMONS-273 in the trunk which is fixed in the branch but due to some reason which is not there
    git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/trunk/modules/XmlSchema@631928 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/ws/commons/schema/constants/Enum.java
+++ src/main/java/org/apache/ws/commons/schema/constants/Enum.java
@@ -39,17 +39,26 @@ public abstract class Enum {
         if (value.equals(Enum.NULL))
             this.value = Enum.NULL;
         else {
-            String values[] = getValues();
-            for (int i = 0; i < values.length; i++) {
-                if (values[i].equals(value)) {
-                    this.value = value;
-                    break;
+            //the value can be a list of space seperated items
+            String possibleValues[] = getValues();
+            String[] valuesToBeTested = value.split("\\s");
+            for (int i = 0; i < valuesToBeTested.length; i++) {
+                for (int j = 0; j < possibleValues.length; j++) {
+                    if (possibleValues[j].equals(valuesToBeTested[i])) {
+                        break;
+                    }
+                    if (i == possibleValues.length - 1)
+                        throw new EnumValueException("Bad Enumeration value '" + value + "'");
                 }
-                if (i == values.length - 1)
-                    throw new EnumValueException("Bad Enumeration value '" + value + "'");
             }
+
+            //when we reach here we have tested all the values to be correct (applicable)
+             this.value = value;
+
+
         }
     }
+
 
     public String getValue() {
         return value;
