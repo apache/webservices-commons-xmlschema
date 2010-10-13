8d1594a5e254ecc4a5e02b0d63a2242db008a9ee
Benson Margulies <bimargulies@apache.org>
2010-10-13 21:24:07 +0000

    add comments to clarify the schema slot in XmlSchemaExternal
    git-svn-id: https://svn.apache.org/repos/asf/webservices/commons/trunk/modules/XmlSchema@1022295 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/ws/commons/schema/XmlSchemaExternal.java
+++ src/main/java/org/apache/ws/commons/schema/XmlSchemaExternal.java
@@ -20,9 +20,10 @@
 package org.apache.ws.commons.schema;
 
 /**
- * An abstract class. Provides information about the included schema.
+ * Common class for include, import, and redefine. All have in common two items:
+ * the location of the referenced schema (required) and an optional
+ * reference to that schema as represented in XmlSchema.
  */
-
 public abstract class XmlSchemaExternal extends XmlSchemaAnnotated {
 
     XmlSchema schema;
@@ -39,6 +40,13 @@ public abstract class XmlSchemaExternal extends XmlSchemaAnnotated {
     public XmlSchema getSchema() {
         return schema;
     }
+
+    /**
+     * Store a reference to an XmlSchema corresponding to this item. This only
+     * case in which this will be read is if you ask the XmlSchemaSerializer
+     * to serialize external schemas.
+     * @param sc schema reference
+     */
     public void setSchema(XmlSchema sc) {
         schema = sc;
     }
